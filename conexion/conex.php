803fc8bbadbf9b5517f63a76fb5ce99f727bcb26
Diego <ldvera41@gmail.com>
2020-04-27 05:31:35 -0500

    insersion y consulta para logeo funcionando

--- conexion/conex.php
+++ conexion/conex.php
@@ -3,22 +3,45 @@
 $conexion = new mysqli('localhost', 'root', '', 'proyecto_dev') or die (mysqli_error($conexion));
 
 
-	if(isset($_POST['btnGuardar'])){
+if(isset($_POST['btnGuardar'])){
 
 
 
-		$USUARIO=$_POST['Usuario'];
-		$APELLIDO_PATERNO=$_POST['UsuarioApePaterno'];
-		$APELLIDO_MATERNO=$_POST['UsuarioApeMaterno'];
-		$CORREO=$_POST['correo'];
-		$CONTRASE=$_POST['contraseña'];
-		$UTIPO = 1;
+	$USUARIO=$_POST['Usuario'];
+	$APELLIDO_PATERNO=$_POST['UsuarioApePaterno'];
+	$APELLIDO_MATERNO=$_POST['UsuarioApeMaterno'];
+	$CORREO=$_POST['correo'];
+	$CONTRASE=$_POST['contraseña'];
+	$UTIPO = 1;
 
 
 
-		$sentencia_sql="insert into usuario (usuario_Nombre, usuario_ApellidoPaterno, usuario_ApellidoMaterno, usuario_Tipo, usuario_Correo, usuario_Contraseña) 
-		values ('$USUARIO','$APELLIDO_PATERNO','$APELLIDO_MATERNO','$UTIPO','$CORREO','$CONTRASE')" ;
-		$conexion -> query($sentencia_sql) or die ($conexion->error);
+	$sentencia_sql="insert into usuario (usuario_Nombre, usuario_ApellidoPaterno, usuario_ApellidoMaterno, usuario_Tipo, usuario_Correo, usuario_Contraseña) 
+	values ('$USUARIO','$APELLIDO_PATERNO','$APELLIDO_MATERNO','$UTIPO','$CORREO','$CONTRASE')" ;
+
+	$conexion -> query($sentencia_sql) or die ($conexion->error);
+}
+
+
+if(isset($_POST['btnLogin'])){
+
+	$correo = $_POST['correoLogin'];
+	$pass =  $_POST['contraseñaLogin'];
+
+	$sql2 = "SELECT * FROM usuario WHERE usuarioCorreo = '$correo' and usuarioContraseña = '$pass'";
+	
+	$result = mysqli_num_rows($sql2);
+
+	if($result == 1)
+	{
+		//header("Location: pagina.html")
+		echo "Bienvenido:" .$correo;
+	}
+	else if ($result == 0) 
+	{
+		header("Location: login.html");
+		//echo "No ingreso"; 
 	}
 
+}
  ?>
---
+++ conexion/login.php
@@ -0,0 +1,38 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "proyecto_dev";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) 
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["correoLogin"];
+$pass = $_POST["contraseñaLogin"];
+
+$query = mysqli_query($conn,"SELECT * FROM usuario WHERE usuario_Nombre = '".$nombre."' and usuario_Contraseña = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+	$n = mysqli_fetch_array($query);
+	$ID_T = $n['usuario_Tipo'];
+	if($ID_T == 0){
+		header("Location: ../Administradores.html");
+	}else if($ID_T == 1){
+		echo "biendenido Usuario!!, disfruta tu experiencia!";
+	}
+}
+else if ($nr == 0) 
+{
+	header("Location: login.html");
+	//echo "No ingreso"; 
+}
+	
+
+
+?>
